9655976674d47bdcae6cb495bc42720a968a9922
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-04-16 13:34:19 +0900

    [Feat] 사이드 콘텐츠바 UI 구현

---
+++ src/pages/CurrentMusical/SidePage.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import SidebarImg from "../../assets/images/sideMusical.svg";
+const SidePage = () => {
+  return (
+    <SideArea>
+      <SideImg src={SidebarImg} alt="사이드바 이미지" />
+      <Explaination>
+        <SideCate>뮤지컬</SideCate>
+        <SideTitle>라이카</SideTitle>
+        <SideDate>2025.03.18 ~ 2025.05.18</SideDate>
+        <SideLocation>연강홀</SideLocation>
+      </Explaination>
+    </SideArea>
+  );
+};
+
+export default SidePage;
+
+const SideArea = styled.div`
+  width: 27%;
+  position: fixed; // 💥 화면에 고정
+  min-height: 100%;
+  top: 70px;
+  left: 0;
+  box-shadow: 5px 5px 18px gray;
+`;
+
+const SideImg = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const Explaination = styled.div`
+  font-family: "Nanum1";
+  margin-left: 40px;
+`;
+
+const SideCate = styled.p`
+  margin-bottom: 18px;
+  font-size: 12px;
+`;
+
+const SideTitle = styled.p`
+  font-size: 24px;
+  margin-bottom: 27px;
+`;
+
+const SideDate = styled.p`
+  font-size: 11px;
+  margin-bottom: 13px;
+  color: #8d8d8d;
+`;
+
+const SideLocation = styled.p`
+  font-size: 11px;
+  color: #8d8d8d;
+`;
