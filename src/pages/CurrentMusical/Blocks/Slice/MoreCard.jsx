d292cd85eb5355d0f51b84e3a1bb14ee74326678
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-05-01 21:24:48 +0900

    [Feat] 카드 컴포넌트에 바뀐 데이터 구조 적용

---
+++ src/pages/CurrentMusical/Blocks/Slice/MoreCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import Dimg from "../../../../assets/images/dummyImg.svg";
+
+const MoreCard = (data) => {
+  console.log(data);
+  return (
+    <Card>
+      <CardImg src={data.data.posterUrl} alt="포스터" />
+      <CardCate>{data.data.genreName}</CardCate>
+      <CardTitle>{data.data.performName}</CardTitle>
+      <CardDate>
+        {data.data.startDate}~{data.data.endDate}
+      </CardDate>
+    </Card>
+  );
+};
+
+export default MoreCard;
+
+const Card = styled.div`
+  width: 253px;
+  height: 458px;
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-bottom: 125px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px gray;
+  cursor: pointer;
+`;
+
+const CardImg = styled.img`
+  width: 253px;
+  height: 320px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+
+const CardCate = styled.p`
+  font-size: 12px;
+  margin-left: 9px;
+  margin-top: 20px;
+  // margin-bottom: 15px;
+`;
+
+const CardTitle = styled.p`
+  font-size: 16px;
+  margin-left: 9px;
+  margin-bottom: 20px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const CardDate = styled.p`
+  font-size: 12px;
+  margin-left: 9px;
+  color: #8d8d8d;
+`;
