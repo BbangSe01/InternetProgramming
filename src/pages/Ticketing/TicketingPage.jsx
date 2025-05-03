dcbc20f96769aad67e2fa65f5f9a7adb418f4392
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-05-03 17:25:23 +0900

    [Chore] 구글 로그인 임시 테스트 중 멈춤

--- src/pages/Ticketing/TicketingPage.jsx
+++ src/pages/Ticketing/TicketingPage.jsx
@@ -1,6 +1,27 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../../apis/axiosInstance";
 const TicketingPage = () => {
+  // 구글 로그인 시험
+  // const [data, setData] = useState([]);
+  // const getLogin = async () => {
+  //   try {
+  //     const response = await axiosInstance.get(`/google/login`);
+  //     console.log(response);
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error("데이터 에러:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const fetchLogin = async () => {
+  //     const result = await getLogin();
+  //     setData(result);
+  //   };
+  //   fetchLogin();
+  // }, []);
+
+  // console.log(data);
   return <div>티켓팅페이지</div>;
 };
 
