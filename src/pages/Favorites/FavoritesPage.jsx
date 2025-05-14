421d07a1f8ee75bb56c68c6ab0e1a90837daa871
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-05-14 20:29:46 +0900

    [Desion] 더보기 페이지/즐겨찾기 페이지 UI 조정

--- src/pages/Favorites/FavoritesPage.jsx
+++ src/pages/Favorites/FavoritesPage.jsx
@@ -6,10 +6,13 @@ import { useLocation } from "react-router";
 
 const FavoritesPage = () => {
   const Favorites = useSelector((state) => state.favorites.items);
-  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지 상단으로 이동
+  }, []);
 
   return (
-    <Screen key={location.key}>
+    <Screen>
       <Title>즐겨찾기</Title>
       <FavoritesArea>
         {Favorites.map((data) => (
@@ -26,16 +29,20 @@ const Screen = styled.div`
   width: 100%;
   font-family: "Nanum1";
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Title = styled.div`
   font-size: 32px;
   margin-top: 100px;
+  margin-bottom: 87px;
 `;
 
 const FavoritesArea = styled.div`
   display: flex;
-  width: 90%;
+  justify-content: space-evenly;
+  width: 1490px;
   flex-wrap: wrap;
+  // background-color: green;
 `;
